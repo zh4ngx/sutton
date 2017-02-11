394d682787f40e12647234e59dda6488f8b837f3
Andy Zhang <signupforandy@gmail.com>
2017-02-11 04:45:31 -0800

    Select 1st action randomly, add noise term

--- bandit.py
+++ bandit.py
@@ -2,13 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 num_actions = 10
-epsilon = 0.001
+epsilon = 0.
 num_iter = 1000
 
 q_star_a = np.random.normal(size=[num_actions])
 R_t_a = np.zeros([num_iter, num_actions])
 Q_t_a = np.zeros([num_iter, num_actions])
 K_a = np.zeros(num_actions, dtype=np.int32)
+
+# First action selected at random
+a = np.random.randint(num_actions)
+Q_t_a[0, a] = q_star_a[a]
 
 for t in range(1, num_iter):
     # Action Selection
@@ -19,9 +23,10 @@ for t in range(1, num_iter):
         a = np.random.randint(num_actions)
 
     # Value Update
+    noise_term = np.random.normal(scale=0.1)
     K_a[a] += 1
     Q_t_a[t] = Q_t_a[t - 1]
-    R_t_a[t, a] = q_star_a[a]
+    R_t_a[t, a] = q_star_a[a] + noise_term
     Q_t_a[t, a] = np.sum(R_t_a[:, a]) / K_a[a]
 
 print("Q values", Q_t_a[-1])
