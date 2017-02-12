b5afaba014bd133ef7b58f1edc98c920026eda22
Andy Zhang <signupforandy@gmail.com>
2017-02-12 15:12:20 -0800

    Fix busted epsilon values

--- bandit-ucb.py
+++ bandit-ucb.py
@@ -6,8 +6,8 @@ np.random.seed(19680801)
 num_actions = 10
 num_trials = 2000
 num_iter = 1000
-epsilon_values = [1e-1, 0]
-c = 1
+epsilon_values = [0, 1e-1]
+c = 2
 epsilons = np.array(epsilon_values * num_trials)
 num_samples = len(epsilons)
 
@@ -54,7 +54,7 @@ for t in range(1, num_iter):
 
 R_t = np.mean(np.sum(R_t_a, axis=1).reshape([num_iter, num_trials, -1]), axis=1)
 plt.subplot(211)
-plt.plot(R_t[:, 0], label='UCB c = 2')
+plt.plot(R_t[:, 0], label='UCB c = %d' % c)
 plt.plot(R_t[:, 1], label='Epsilon Greedy e = 0.1')
 plt.xlabel('Steps')
 plt.ylabel('Average reward')
