aafec898e9b9cd0caf11bf9e75e4695f72ea4102
Andy Zhang <signupforandy@gmail.com>
2017-02-11 21:43:30 -0800

    Refactor plot, add legend, clean up lens

--- bandit-softmax.py
+++ bandit-softmax.py
@@ -6,15 +6,16 @@ np.random.seed(19680801)
 num_actions = 10
 num_trials = 2000
 num_iter = 10000
-tau_values = [np.exp(0), np.exp(-2), np.exp(-4)]
+tau_values = [np.exp(0), np.exp(-1), np.exp(-2), np.exp(-3)]
 taus = np.array(tau_values * num_trials)
+num_samples = len(taus)
 
 q_star_a = np.repeat(np.random.normal(size=[num_actions, num_trials]), len(tau_values), axis=1)
 optimal_action = np.argmax(q_star_a, axis=0)
-optimal_actions = np.zeros([num_iter, len(taus)], dtype=np.int32)
-R_t_a = np.zeros([num_iter, num_actions, len(taus)])
-Q_a = np.zeros([num_actions, len(taus)])
-K_a = np.zeros([num_actions, len(taus)], dtype=np.int32)
+optimal_actions = np.zeros([num_iter, num_samples], dtype=np.int32)
+R_t_a = np.zeros([num_iter, num_actions, num_samples])
+Q_a = np.zeros([num_actions, num_samples])
+K_a = np.zeros([num_actions, num_samples], dtype=np.int32)
 
 # The first action is always assumed to be the action at index 0
 # Absent prior knowledge, this is equivalent to a random choice
@@ -25,12 +26,12 @@ for t in range(1, num_iter):
     sum_energy = np.sum(energy, axis=0, keepdims=True)
     softmax = energy / sum_energy
     cdf = np.cumsum(softmax, axis=0)
-    actions = np.argmin(np.random.random_sample(len(taus)) > cdf, axis=0)
-    action_idx = actions, np.arange(len(taus))
+    actions = np.argmin(np.random.random_sample(num_samples) > cdf, axis=0)
+    action_idx = actions, np.arange(num_samples)
     optimal_actions[t, actions == optimal_action] += 1
 
     # Sample Environment
-    noise_term = np.random.normal(scale=1., size=len(taus))
+    noise_term = np.random.normal(scale=1., size=num_samples)
     R_t_a[t][action_idx] = q_star_a[action_idx] + noise_term
 
     # Update Estimate
@@ -44,15 +45,14 @@ for t in range(1, num_iter):
 R_t = np.mean(np.sum(R_t_a, axis=1).reshape([num_iter, num_trials, -1]), axis=1)
 A_t = np.mean(optimal_actions.reshape([num_iter, num_trials, -1]), axis=1)
 plt.subplot(211)
-plt.plot(R_t[:, 0], 'r')
-plt.plot(R_t[:, 1], 'g')
-plt.plot(R_t[:, 2], 'b')
+for eps in range(len(tau_values)):
+    plt.plot(R_t[:, eps], label="tau = %f" % tau_values[eps])
 plt.xlabel('Steps')
 plt.ylabel('Average reward')
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=3, mode="expand", borderaxespad=0.)
 plt.subplot(212)
-plt.plot(A_t[:, 0], 'r')
-plt.plot(A_t[:, 1], 'g')
-plt.plot(A_t[:, 2], 'b')
+plt.plot(np.mean(optimal_actions.reshape([num_iter, num_trials, -1]), axis=1))
 plt.xlabel('Steps')
 plt.ylabel('Optimal action')
 plt.show()
--- bandit.py
+++ bandit.py
@@ -6,30 +6,31 @@ np.random.seed(19680801)
 num_actions = 10
 num_trials = 2000
 num_iter = 10000
-epsilon_values = [0., 0.1, 0.01]
+epsilon_values = [1e0, 1e-1, 1e-2, 1e-3]
 epsilons = np.array(epsilon_values * num_trials)
+num_samples = len(epsilons)
 
 q_star_a = np.repeat(np.random.normal(size=[num_actions, num_trials]), len(epsilon_values), axis=1)
 optimal_action = np.argmax(q_star_a, axis=0)
-optimal_actions = np.zeros([num_iter, len(epsilons)], dtype=np.int32)
-R_t_a = np.zeros([num_iter, num_actions, len(epsilons)])
-Q_a = np.zeros([num_actions, len(epsilons)])
-K_a = np.zeros([num_actions, len(epsilons)], dtype=np.int32)
+optimal_actions = np.zeros([num_iter, num_samples], dtype=np.int32)
+R_t_a = np.zeros([num_iter, num_actions, num_samples])
+Q_a = np.zeros([num_actions, num_samples])
+K_a = np.zeros([num_actions, num_samples], dtype=np.int32)
 
 # The first action is always assumed to be the action at index 0
 # Absent prior knowledge, this is equivalent to a random choice
 
 for t in range(1, num_iter):
     # Select Action
-    is_greedy = np.random.random(len(epsilons)) < (1 - epsilons)
+    is_greedy = np.random.random(num_samples) < (1 - epsilons)
     greedy_actions = np.argmax(Q_a, axis=0)
-    random_actions = np.random.randint(num_actions, size=len(epsilons))
+    random_actions = np.random.randint(num_actions, size=num_samples)
     actions = np.where(is_greedy, greedy_actions, random_actions)
-    action_idx = actions, np.arange(len(epsilons))
+    action_idx = actions, np.arange(num_samples)
     optimal_actions[t, actions == optimal_action] += 1
 
     # Sample Environment
-    noise_term = np.random.normal(scale=1., size=len(epsilons))
+    noise_term = np.random.normal(scale=1., size=num_samples)
     R_t_a[t][action_idx] = q_star_a[action_idx] + noise_term
 
     # Update Estimate
@@ -43,9 +44,12 @@ for t in range(1, num_iter):
 
 R_t = np.mean(np.sum(R_t_a, axis=1).reshape([num_iter, num_trials, -1]), axis=1)
 plt.subplot(211)
-plt.plot(R_t)
+for eps in range(len(epsilon_values)):
+    plt.plot(R_t[:, eps], label="eps = %f" % epsilon_values[eps])
 plt.xlabel('Steps')
 plt.ylabel('Average reward')
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=3, mode="expand", borderaxespad=0.)
 plt.subplot(212)
 plt.plot(np.mean(optimal_actions.reshape([num_iter, num_trials, -1]), axis=1))
 plt.xlabel('Steps')
