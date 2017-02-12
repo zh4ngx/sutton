8a966923f859c0a854b2230fe3a3d907d9f2ed37
Andy Zhang <signupforandy@gmail.com>
2017-02-12 00:21:58 -0800

    Correct optimistic bandit to be fully greedy per ex

--- bandit-optimistic.py
+++ bandit-optimistic.py
@@ -6,11 +6,11 @@ np.random.seed(19680801)
 num_actions = 10
 num_trials = 2000
 num_iter = 1000
-epsilon = 1e-1
-num_configs = 2
-num_samples = num_configs * num_trials
+epsilon_values = [1e-1, 0]
+epsilons = np.array(epsilon_values * num_trials)
+num_samples = len(epsilons)
 
-q_star_a = np.repeat(np.random.normal(size=[num_actions, num_trials]), num_configs, axis=1)
+q_star_a = np.repeat(np.random.normal(size=[num_actions, num_trials]), len(epsilon_values), axis=1)
 optimal_action = np.argmax(q_star_a, axis=0)
 optimal_actions = np.zeros([num_iter, num_samples], dtype=np.int32)
 R_t_a = np.zeros([num_iter, num_actions, num_samples])
@@ -23,7 +23,7 @@ K_a = np.zeros([num_actions, num_samples], dtype=np.int32)
 
 for t in range(1, num_iter):
     # Select Action
-    is_greedy = np.random.random(num_samples) < (1 - epsilon)
+    is_greedy = np.random.random(num_samples) < (1 - epsilons)
     greedy_actions = np.argmax(Q_a, axis=0)
     random_actions = np.random.randint(num_actions, size=num_samples)
     actions = np.where(is_greedy, greedy_actions, random_actions)
@@ -47,7 +47,7 @@ for t in range(1, num_iter):
 R_t = np.mean(np.sum(R_t_a, axis=1).reshape([num_iter, num_trials, -1]), axis=1)
 plt.subplot(211)
 plt.plot(R_t[:, 0], label='realistic, e = 0.1')
-plt.plot(R_t[:, 1], label='optimistic, e = 0.1')
+plt.plot(R_t[:, 1], label='optimistic, e = 0')
 plt.xlabel('Steps')
 plt.ylabel('Average reward')
 plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
